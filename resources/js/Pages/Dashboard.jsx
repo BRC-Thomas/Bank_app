8f2c2f125ebb56821713e73e7e4cb482b221b0c2
BRC-Thomas <thomasbaracani@gmail.com>
2023-07-17 21:06:20 +0200

    🎉 Initial commit

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,25 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+import { FaBeer } from 'react-icons/fa';
+
+export default function Dashboard({ auth }) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        >
+            <Head title="Dashboard" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 mt-4 text-gray-900">DIV insideCOMPONENTS</div>
+                    </div>
+                  {/*  <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4">
+                        <div className="p-6 mt-4 text-gray-900">DIV insideCOMPONENTS</div>
+                    </div>*/}
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
